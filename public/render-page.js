69107e19aa2d5039abd7703c60771cf7b1cab2c1
Mike O'Brien <michael@mkobdesign.com>
2020-12-03 20:57:36 -0500

    basics

--- public/render-page.js
+++ public/render-page.js
@@ -1,12 +1,12 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("/Users/michaelobrien/revive-client-portal/node_modules/@reach/router/index.js"), require("fs"), require("/Users/michaelobrien/revive-client-portal/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
+		module.exports = factory(require("/Users/michaelobrien/revive-healthcare-member-portal/node_modules/@reach/router/index.js"), require("fs"), require("/Users/michaelobrien/revive-healthcare-member-portal/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
 	else if(typeof define === 'function' && define.amd)
-		define("lib", ["/Users/michaelobrien/revive-client-portal/node_modules/@reach/router/index.js", "fs", "/Users/michaelobrien/revive-client-portal/node_modules/lodash/merge.js", "path", "react", "react-dom/server", "react-helmet"], factory);
+		define("lib", ["/Users/michaelobrien/revive-healthcare-member-portal/node_modules/@reach/router/index.js", "fs", "/Users/michaelobrien/revive-healthcare-member-portal/node_modules/lodash/merge.js", "path", "react", "react-dom/server", "react-helmet"], factory);
 	else if(typeof exports === 'object')
-		exports["lib"] = factory(require("/Users/michaelobrien/revive-client-portal/node_modules/@reach/router/index.js"), require("fs"), require("/Users/michaelobrien/revive-client-portal/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
+		exports["lib"] = factory(require("/Users/michaelobrien/revive-healthcare-member-portal/node_modules/@reach/router/index.js"), require("fs"), require("/Users/michaelobrien/revive-healthcare-member-portal/node_modules/lodash/merge.js"), require("path"), require("react"), require("react-dom/server"), require("react-helmet"));
 	else
-		root["lib"] = factory(root["/Users/michaelobrien/revive-client-portal/node_modules/@reach/router/index.js"], root["fs"], root["/Users/michaelobrien/revive-client-portal/node_modules/lodash/merge.js"], root["path"], root["react"], root["react-dom/server"], root["react-helmet"]);
+		root["lib"] = factory(root["/Users/michaelobrien/revive-healthcare-member-portal/node_modules/@reach/router/index.js"], root["fs"], root["/Users/michaelobrien/revive-healthcare-member-portal/node_modules/lodash/merge.js"], root["path"], root["react"], root["react-dom/server"], root["react-helmet"]);
 })(this, function(__WEBPACK_EXTERNAL_MODULE__reach_router__, __WEBPACK_EXTERNAL_MODULE_fs__, __WEBPACK_EXTERNAL_MODULE_lodash_merge__, __WEBPACK_EXTERNAL_MODULE_path__, __WEBPACK_EXTERNAL_MODULE_react__, __WEBPACK_EXTERNAL_MODULE_react_dom_server__, __WEBPACK_EXTERNAL_MODULE_react_helmet__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -33811,9 +33811,9 @@ if (false) {} else {
 /***/ }),
 
 /***/ "@reach/router":
-/*!************************************************************************************************!*\
-  !*** external "/Users/michaelobrien/revive-client-portal/node_modules/@reach/router/index.js" ***!
-  \************************************************************************************************/
+/*!***********************************************************************************************************!*\
+  !*** external "/Users/michaelobrien/revive-healthcare-member-portal/node_modules/@reach/router/index.js" ***!
+  \***********************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -33833,9 +33833,9 @@ module.exports = __WEBPACK_EXTERNAL_MODULE_fs__;
 /***/ }),
 
 /***/ "lodash/merge":
-/*!*****************************************************************************************!*\
-  !*** external "/Users/michaelobrien/revive-client-portal/node_modules/lodash/merge.js" ***!
-  \*****************************************************************************************/
+/*!****************************************************************************************************!*\
+  !*** external "/Users/michaelobrien/revive-healthcare-member-portal/node_modules/lodash/merge.js" ***!
+  \****************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
