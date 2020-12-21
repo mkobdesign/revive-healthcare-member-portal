e2f9533ff13b7cb67476458b3e857912fa148d68
Mike O'Brien <michael@mkobdesign.com>
2020-12-21 16:01:46 -0500

    taking out account page

--- plugins/gatsby-plugin-auth0/gatsby-node.js
+++ plugins/gatsby-plugin-auth0/gatsby-node.js
@@ -4,7 +4,7 @@ exports.createPages = ({ graphql, actions }) => {
   const component = path.resolve(`plugins/gatsby-plugin-auth0/LoginCallback.js`)
   const { createPage } = actions
   createPage({
-    path: '/login_callback',
+    path: '/',
     component,
   })
 }
