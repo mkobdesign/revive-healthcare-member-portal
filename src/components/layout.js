098eda71ca1cc8394d69aa77ef3404d505462a08
Ben Quirk <ben.quirk@edlogics.com>
2021-05-26 10:51:16 -0400

    wired up google tag manager to track external link clicks

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,10 +14,17 @@ import "fontsource-roboto/500-normal.css";
 
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
-import Row from 'react-bootstrap/Row'
+import Row from 'react-bootstrap/Row';
 
-import Sidebar from "./sidebar"
+import Sidebar from "./sidebar";
 import "../styles/main.scss";
+
+import TagManager from 'react-gtm-module';
+const tagManagerArgs = {
+  gtmId: 'GTM-KBDHFS2'
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const Layout = ({ children }) => (
   <StaticQuery
